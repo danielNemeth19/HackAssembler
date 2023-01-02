f7480031b06e708d55451448f3fdd74fcfd61983
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2023-01-02 10:47:22 +0100

    SymbolTable created as struct with recevier methods

--- Assembler.go
+++ Assembler.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -15,25 +16,53 @@ func checkError(e error) {
 	}
 }
 
-type AssemblyCode struct {
-	lineNumber int
-	codeLines  []string
+type SymbolTable struct {
+	table map[string]int
 }
 
-func readHackFile(fp string) AssemblyCode {
+func (symbolTable *SymbolTable) initialize() {
+	initValues := map[string]int{
+		"SP": 0, "LCL": 1, "ARG": 2, "THIS": 3, "THAT": 4, "SCREEN": 16384, "KBD": 24576,
+	}
+	for i := 0; i < 16; i++ {
+		key := "R" + strconv.Itoa(i)
+		initValues[key] = i
+	}
+	symbolTable.table = initValues
+}
+
+func (symbolTable *SymbolTable) storeSymbol(codeSnippet string, counter int) {
+	symbol := codeSnippet[1 : len(codeSnippet)-1]
+	symbolTable.table[symbol] = counter
+}
+
+func (symbolTable *SymbolTable) getAddress(symbol string) (address int, found bool) {
+	address, found = symbolTable.table[symbol]
+	return address, found
+}
+
+func readHackFile(fp string) ([]string, SymbolTable) {
+	var symbolTable SymbolTable
+	symbolTable.initialize()
 	file, err := os.Open(fp)
 	checkError(err)
 	scanner := bufio.NewScanner(file)
-	code := AssemblyCode{}
+	var assemblyCode []string
+	counter := -1
 	for scanner.Scan() {
 		line := scanner.Text()
 		subs := strings.Split(line, "//")
 		if len(subs[0]) > 0 {
-			code.codeLines = append(code.codeLines, strings.TrimSpace(subs[0]))
-			code.lineNumber = code.lineNumber + 1
+			codeSnippet := strings.TrimSpace(subs[0])
+			if strings.HasPrefix(codeSnippet, "(") {
+				symbolTable.storeSymbol(codeSnippet, counter+1)
+			} else {
+				counter += 1
+				assemblyCode = append(assemblyCode, codeSnippet)
+			}
 		}
 	}
-	return code
+	return assemblyCode, symbolTable
 }
 
 func parseArg() string {
@@ -50,9 +79,17 @@ func parseArg() string {
 
 func main() {
 	fp := parseArg()
-	assemblyCode := readHackFile(fp)
-	for index := range assemblyCode.codeLines {
-		fmt.Println(assemblyCode.codeLines[index], index)
+	assemblyCode, symbolTable := readHackFile(fp)
+	for index, code := range assemblyCode {
+		fmt.Println(index, code)
 	}
-	fmt.Printf("Number of code lines: %d\n", assemblyCode.lineNumber)
+	fmt.Printf("Number of code lines: %d\n", len(assemblyCode))
+	fmt.Printf("Number items in symbol table: %d\n", len(symbolTable.table))
+
+	value, flag := symbolTable.getAddress("novalue")
+	fmt.Printf("Value: %d -- flag %v\n", value, flag)
+
+	value, flag = symbolTable.getAddress("OUTPUT_FIRST")
+	fmt.Printf("Value: %d -- flag %v\n", value, flag)
+
 }
