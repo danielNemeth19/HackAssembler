b9bfd7456c9ba261333cc2f23fc9836784a6080c
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2023-01-19 00:20:55 +0100

    Adds IsLabel, IsAInstruction and SetDestinationFile tests to Parser

--- parser_test.go
+++ parser_test.go
@@ -1,10 +1,30 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+type inputTable struct {
+	code           string
+	isLabel        bool
+	isAInstruction bool
+}
+
+func makeParser() Parser {
+	return Parser{SourceFile: "test.asm"}
+}
+
+func TestParser_SetDestinationFile(t *testing.T) {
+	p := makeParser()
+	expectedPath := "test.hack"
+	destPath := p.SetDestinationFile()
+	if destPath != expectedPath {
+		t.Errorf("Path incorrect: got %s, expected: %s", destPath, expectedPath)
+	}
+}
 
 func TestParser_IsLabel_False(t *testing.T) {
-	var p Parser
-	p = Parser{SourceFile: "test.asm"}
+	p := makeParser()
 	res := p.IsLabel("not a label")
 	if res == true {
 		t.Errorf("Result incorrect: got %v, expected false\n", res)
@@ -12,8 +32,7 @@ func TestParser_IsLabel_False(t *testing.T) {
 }
 
 func TestParser_IsLabel_True(t *testing.T) {
-	var p Parser
-	p = Parser{SourceFile: "test.asm"}
+	p := makeParser()
 	res := p.IsLabel("(thisIsALabel)")
 	if res != true {
 		t.Errorf("Result incorrect: got %v, expected true\n", res)
@@ -21,21 +40,28 @@ func TestParser_IsLabel_True(t *testing.T) {
 }
 
 func TestParser_IsLabel_TestTable(t *testing.T) {
-	var p Parser
-	p = Parser{SourceFile: "test.asm"}
-	table := []struct {
-		code    string
-		isLabel bool
-	}{
-		{"(label1)", true},
-		{"A=M", false},
-		{"(label2)", true},
-		{"0;JMP", false},
+	p := makeParser()
+	table := []inputTable{
+		{code: "(label1)", isLabel: true},
+		{code: "A=M", isLabel: false},
+		{code: "(label2)", isLabel: true},
+		{code: "0;JMP", isLabel: false},
 	}
 	for _, data := range table {
 		res := p.IsLabel(data.code)
 		if res != data.isLabel {
 			t.Errorf("Result incorrect: got %v, expected %v\n", res, data.isLabel)
+		}
+	}
+}
+
+func TestParser_IsAInstruction(t *testing.T) {
+	p := makeParser()
+	table := []inputTable{{code: "@256", isAInstruction: true}, {code: "0;JMP", isAInstruction: false}}
+	for _, data := range table {
+		res := p.IsAInstruction(data.code)
+		if res != data.isAInstruction {
+			t.Errorf("Result incorrect: got %v, expected %v\n", res, data.isAInstruction)
 		}
 	}
 }
